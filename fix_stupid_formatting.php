ad0b62892895add8ff24b305c37e421656ef1e7a
Dylan K. Taylor <odigiman@gmail.com>
2016-12-27 15:54:11 +0000

    Fix parser errors (Somebody tell Mojang you can't use comments in json >_<)

--- fix_stupid_formatting.php
+++ fix_stupid_formatting.php
@@ -2,13 +2,16 @@
 
 $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator(__DIR__));
 foreach($iterator as $file){
-	//var_dump($file->getFilename());
 	if(substr($file->getFilename(), -5) === ".json"){
-		file_put_contents($file->getPath() . DIRECTORY_SEPARATOR . $file->getFilename(), str_replace("    ", "  ", json_encode(json_decode(file_get_contents($file->getPath() . DIRECTORY_SEPARATOR . $file->getFilename())), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES)));
+		$lines = implode('', array_map(function($line){
+			return trim(explode('//', $line)[0]);
+		}, file($file->getPath() . DIRECTORY_SEPARATOR . $file->getFilename())));
+
+		file_put_contents($file->getPath() . DIRECTORY_SEPARATOR . $file->getFilename(), str_replace("    ", "  ", json_encode(json_decode($lines), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES)));
 		echo "Processed " . $file->getPath() . DIRECTORY_SEPARATOR . $file->getFilename() . PHP_EOL;
 	}
 }
 
-echo "Done" .PHP_EOL;
+echo "Done" . PHP_EOL;
 
 
