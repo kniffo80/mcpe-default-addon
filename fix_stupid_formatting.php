75a3524f013c73e767c4f44e3b52e1d33ce754ce
Dylan K. Taylor <odigiman@gmail.com>
2016-12-27 15:25:49 +0000

    Fix filename slashes

--- fix_stupid_formatting.php
+++ fix_stupid_formatting.php
@@ -4,7 +4,7 @@ $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator(__DIR__
 foreach($iterator as $file){
 	//var_dump($file->getFilename());
 	if(substr($file->getFilename(), -5) === ".json"){
-		file_put_contents($file->getPath() . DIRECTORY_SEPARATOR . $file->getFilename(), str_replace("    ", "  ", json_encode(json_decode(file_get_contents($file->getPath() . DIRECTORY_SEPARATOR . $file->getFilename())), JSON_PRETTY_PRINT)));
+		file_put_contents($file->getPath() . DIRECTORY_SEPARATOR . $file->getFilename(), str_replace("    ", "  ", json_encode(json_decode(file_get_contents($file->getPath() . DIRECTORY_SEPARATOR . $file->getFilename())), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES)));
 		echo "Processed " . $file->getPath() . DIRECTORY_SEPARATOR . $file->getFilename() . PHP_EOL;
 	}
 }
